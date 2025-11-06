cd2d60d36b0deefb7165865d4250a9ef8793ba7e
Mathieu Carbou <mathieu.carbou@gmail.com>
2025-11-06 16:35:54 +0100

    v7.3.0

--- src/MycilaConfig.h
+++ src/MycilaConfig.h
@@ -14,9 +14,9 @@
   #include <ArduinoJson.h>
 #endif
 
-#define MYCILA_CONFIG_VERSION          "7.2.0"
+#define MYCILA_CONFIG_VERSION          "7.3.0"
 #define MYCILA_CONFIG_VERSION_MAJOR    7
-#define MYCILA_CONFIG_VERSION_MINOR    2
+#define MYCILA_CONFIG_VERSION_MINOR    3
 #define MYCILA_CONFIG_VERSION_REVISION 0
 
 // suffix to use for a setting key enabling a feature
